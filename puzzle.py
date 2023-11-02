ecf5c42fe6fda154b46c58502919551891cf5f3f
Artem Krutko <291dedlol5484@gamil.com>
2023-11-03 00:00:07 +0200

    create second check

--- puzzle.py
+++ puzzle.py
@@ -15,6 +15,19 @@ def validate_board(board: list[str])-> bool:
  "  2  ****"\
 ])
     False
+
+    >>> validate_board([\
+ "**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****"\
+])
+    False
     '''
 
     for row in board:
@@ -25,6 +38,13 @@ def validate_board(board: list[str])-> bool:
                     return False
                 numbers += symb
 
+    for i in range(len(board[0])):
+        numbers = ''
+        for row in board:
+            if row[i].isdigit():
+                if row[i] in numbers:
+                    return False
+                numbers += row[i]
     return True
 
 if __name__ == '__main__':
