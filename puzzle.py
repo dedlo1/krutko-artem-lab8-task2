71599f2c80f0a75e98ae4abe788143479a21d7df
Artem Krutko <291dedlol5484@gamil.com>
2023-11-02 23:58:42 +0200

    create func, add doctest

---
+++ puzzle.py
@@ -0,0 +1,9 @@
+''' Puzzle '''
+
+def validate_board(board: list[str])-> bool:
+    ''' Checking board if it valid '''
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
